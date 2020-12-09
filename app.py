d2615476c8e648010c1ea415a2310a717eefe45c
Panda64 <JaylenSchelb@yahoo.com>
2020-12-08 21:26:55 -0800

    Update deployment settings

--- app.py
+++ app.py
@@ -21,10 +21,8 @@ app = Flask(__name__)
 
 
 
-client = pymongo.MongoClient(f"mongodb+srv://{MONGODB_USERNAME}:{MONGODB_PASSWORD}@cluster0.elrb9.mongodb.net/{MONGODB_DBNAME}?retryWrites=true&w=majority")
-db = client.test
-
-mongo = PyMongo(app)
+mongo = pymongo.MongoClient(f"mongodb+srv://{MONGODB_USERNAME}:{MONGODB_PASSWORD}@cluster0.elrb9.mongodb.net/{MONGODB_DBNAME}?retryWrites=true&w=majority")
+db = mongo.test
 
 
 ############################################################
