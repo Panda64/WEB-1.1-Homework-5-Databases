51e23c44c172a4d9052e0057bc84107ede4d1ea7
Panda64 <JaylenSchelb@yahoo.com>
2020-12-08 18:32:41 -0800

    Update deployment settings

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, redirect, render_template, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from pymongo import MongoClient
+from dotenv import load_dotenv
 
 ############################################################
 # SETUP
